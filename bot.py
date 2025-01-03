da08c18b66e7675978f8c6ceb29e7fd5a13a8f03
helowitch <heloiseperat@gmail.com>
2025-01-03 18:16:52 +0100

    Fix Callbackcontext

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 import re
 
 # Votre token
@@ -9,12 +9,11 @@ async def start(update: Update, context: CallbackContext) -> None:
     print("Commande /start reçue.")  # Ajout d'un message pour savoir si /start est appelé
     await update.message.reply_text('Bonjour! Je suis votre bot.')
 
-async def respond_to_where(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def respond_to_where(update: Update, context: CallbackContext) -> None:
     if update.message and update.message.text:  # Vérifie que le message existe et est du texte
         message_text = update.message.text.lower()  # On met tout en minuscule pour éviter la casse
         if message_text.endswith("où") or message_text == "c'est où":
             await update.message.reply_text("À Montélimar")
-
 
 def main() -> None:
     print("Démarrage du bot...")  # Ajoutez cette ligne pour confirmer que le bot démarre
